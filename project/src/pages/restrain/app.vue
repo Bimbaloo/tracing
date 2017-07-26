e6704d57519252b4617afc07b3275b6579b8f053
mazao <mazao@zjrealtech.com>
2017-07-26 15:52:14 +0800

    遏制页面--缺查询和路由等功能

--- project/src/pages/restrain/app.vue
+++ project/src/pages/restrain/app.vue
@@ -9,53 +9,14 @@
 							<div class='radio'>
 								<el-radio :label="radio.key" v-for="radio in radioList">{{radio.groupName}}</el-radio>
 							</div>
-							<el-form :inline="true" :model="radio.ruleForm"  :class="[{ hide: radioa === radio.key }, 'demo-form-inline','form-inline']" v-for="radio in radioList">
+							<el-form :inline="true" ref="radio.ruleForm" :model="radio.ruleForm"  :class="[{ hide: radioa === radio.key }, 'demo-form-inline','form-inline']" v-for="radio in radioList">
 								 <el-form-item v-for="item in radio.groupItems" :label="item.placeholder">
-									<component :is="`v-${item.type}`" :form-data="item.type" :placeholder-data="item.placeholder" :key-data="item.type"></component> 
+									 <component :is="`v-${item.type}`" :form-data="radio.ruleForm" :placeholder-data="item.placeholder" :key-data="item.itemCode"></component>  
 								</el-form-item> 
 								<el-form-item>
-									<el-button type="primary" class='btn' @click="onSubmit">查询</el-button>
+									<el-button type="primary" class='btn' @click="submitForm(radio.ruleForm)">查询</el-button>
 								</el-form-item>
 							</el-form>
-							<!-- <el-form :inline="true" :model="formInline" :class="[{ hide: radio !=='1' }, 'demo-form-inline','form-inline']" v-for="radio in radios">
-								<el-form-item label="物料:">
-									<el-select v-model="value" placeholder="请选择物料">
-										<el-option
-											v-for="item in options"
-											:key="item.value"
-											:label="item.label"
-											:value="item.value">
-										</el-option>
-									</el-select>
-								</el-form-item>
-								<el-form-item label="批次:">
-									<el-input v-model="input" placeholder="请输入批次"></el-input>
-								</el-form-item>
-								<el-form-item>
-									<el-button type="primary" class='btn' @click="onSubmit">查询</el-button>
-								</el-form-item>
-							</el-form>
-							<el-form :inline="true" :model="formInline" :class="[{ hide: radio !=='2' }, 'demo-form-inline','form-inline']">
-								<el-form-item label="物料:">
-									<el-select v-model="value" placeholder="请选择物料">
-										<el-option
-											v-for="item in options"
-											:key="item.value"
-											:label="item.label"
-											:value="item.value">
-										</el-option>
-									</el-select>
-								</el-form-item>
-								<el-form-item label="开始时间:">
-									<el-date-picker v-model="startTime" type="datetime" placeholder="选择日期时间"></el-date-picker>
-								</el-form-item>
-								<el-form-item label="结束时间:">
-									<el-date-picker v-model="endTime" type="datetime" placeholder="选择日期时间"></el-date-picker>
-								</el-form-item>
-								<el-form-item>
-									<el-button type="primary" class='btn' @click="onSubmit">查询</el-button>
-								</el-form-item>
-							</el-form> -->
 						</el-radio-group>
 					</el-tab-pane>
 					<el-tab-pane label="遏制列表" activeName="second">遏制列表</el-tab-pane>
@@ -71,7 +32,7 @@
 
 <script>
 	import header from "components/header/header.vue"
-	//import panel from "components/panel/panel.vue"
+
 	import Input from 'components/basic/input.vue'
     import Select from 'components/basic/select.vue'
     import DateTime from 'components/basic/dateTime.vue'
@@ -134,49 +95,168 @@
                         }
 					],
 					ruleForm:{
-						materialCode:'',
+						personCode:'',
 						startTime:'',
 						endTime:''
 					}
 				}],
 
 				activeName: 'first',
-				radioa: '1',
-				formInline: {
-					user: '',
-					region: ''
-				},
-				startTime:'',
-				endTime:'',
-				options: [{
-					value: '选项1',
-					label: '黄金糕'
-					}, {
-					value: '选项2',
-					label: '双皮奶'
-					}, {
-					value: '选项3',
-					label: '蚵仔煎'
-					}, {
-					value: '选项4',
-					label: '龙须面'
-					}, {
-					value: '选项5',
-					label: '北京烤鸭'
-				}],
-				value: '',
-				input:''
+				radioa: '1'
+				
 			}
 		},
 		// 计算属性。
-		computed: {},
+		computed: {
+            ruleForms: function() {
+                let oFormData = {};
+                this.items.forEach(o => {
+                	oFormData[o.key] = this.active.keys[o.key] || '';
+                });
+                return oFormData;
+            },
+            rules: function() {
+            	// 验证条码。
+            	let _that = this,
+            		oForm = this.ruleForm,
+            		oKeys = this.keys,
+            		// 溯源及追踪的参数。
+            		aParams = ["barcode","materialCode","equipmentCode","doCode"],
+            		// 验证条码
+	            	validateBarcode = (rule, value, callback) => {
+	            		if(!value) {
+	            			callback(new Error("请输入条码"));
+	            		}else {
+	            			callback();
+	            		}
+	            	},
+	            	// 验证批次。
+	            	validateBatch = (rule, value, callback) => {
+	            		if(!value) {
+	            			callback(new Error("请输入批次"));
+	            		}else {
+	            			callback();
+	            		}
+	            	},
+	            	// 验证物料。
+	            	validateMaterialcode = (rule, value, callback) => {
+	            		if(!value) {
+	            			callback(new Error("请输入物料"));
+	            		}else {
+	            			callback();
+	            		}
+	            	},
+            		// 验证开始时间。
+            	    validateStartTime = (rule, value, callback) => {
+	            		if(!value) {
+	            			callback(new Error("请输入开始时间"));
+	            		}else {
+	            			callback();
+	            		}
+	            	},
+            		// 验证结束时间。
+            		validateEndTime = (rule, value, callback) => {
+	            		let sStart = oForm.startTime;
+	            		if(!value) {
+	            			callback(new Error("请输入结束时间"));
+	            		}else if(sStart && sStart > value) {
+	            			// 如果开始时间存在，而且开始时间大于结束时间。
+	            			callback(new Error("结束时间必须大于开始时间"));
+	            		}else {
+	            			callback();
+	            		}
+	            	};
+	            	
+	            // 验证是否存在
+	            var validateParam = (rule, value, callback) => {
+	            	// 当前筛选条件中需判断的参数。
+	            	let oJudge = {},
+	            		aJudgeName = [];
+	            	
+	            	// 循环处理。
+	            	for (let param in oKeys) {
+	            		// 当前参数存在。
+	            		if(aParams.includes(param)) {
+	            			oJudge[param] = oForm[param];
+	            			aJudgeName.push(_that.getNameByKey(param));
+	            		}
+	            	}
+	            	
+	            	// 如果judge的参数中所有的都为空。则提示
+	            	if(window.Rt.utils.getObjectValues(oJudge).some(o=>o)) {
+	            		// 只要一个有数据。
+	            		callback();
+	            	}else {
+	            		// 否则。
+	            		callback(new Error(aJudgeName.join(",")+(aJudgeName.length == 1?"不能为空":"必填其中一项")));
+	            	}
+	            	
+	            },
+	            // 结束时间。
+	            validateTime = (rule, value, callback) => {
+	            	if(value && oForm.startTime && value < oForm.startTime) {
+	            		// 开始时间和结束时间都存在,且结束时间小。
+	            		callback(new Error("结束时间要大于开始时间"));
+	            	}else {
+	            		callback();
+	            	}
+	            };
+            	
+            	// 所有规则。
+            	var oAllRules =  {
+            		// 查出库。
+            		"stock": {
+	                	"barcode": [{validator: validateBarcode, trigger: "change"}],
+	                	"batchNo": [{validator: validateBatch,trigger: "change"}],
+	                	"materialCode": [{validator: validateMaterialcode,trigger: "change"}],
+	                	// 开始时间。
+	                	"startTime": [{validator: validateStartTime, trigger: "change"}],
+	                	// 结束时间。
+	                	"endTime": [{validator: validateEndTime, trigger: "change"}]
+            		},
+            		// 溯源。
+            		"trace": {
+            			"barcode": [{validator: validateParam, trigger: "change"}],
+            			"materialCode": [{validator: validateParam, trigger: "change"}],
+            			"equipmentCode": [{validator: validateParam, trigger: "change"}],
+            			"doCode": [{validator: validateParam, trigger: "change"}],
+            			// 结束时间。
+            			"endTime": [{validator: validateTime, trigger: "change"}]
+            		},
+            		// 追踪。
+            		"track": {
+            			"barcode": [{validator: validateParam,trigger: "change"}],
+            			"materialCode": [{validator: validateParam,trigger: "change"}],
+            			"equipmentCode": [{validator: validateParam,trigger: "change"}],
+            			"doCode": [{validator: validateParam,trigger: "change"}],
+            			// 结束时间。
+            			"endTime": [{validator: validateTime, trigger: "change"}]
+            		},
+            		// 履历。
+            		"resume": {
+            			"barcode": [{validator: validateBarcode, trigger: "change"}]
+            		}
+                };
+                
+            	// 根据当前配的返回对应的规则。
+            	let oRule = {};
+				for(let key in this.keys) {
+					if(oAllRules[this.tab][key]) {
+						oRule[key] = oAllRules[this.tab][key];
+					}
+				}
+				
+				// 为了将页面中*都不存在。
+                return oRule;
+            }
+        },
 		// 创建时处理。mounted
 		created() {},
 		// 页面方法。
 		methods: {
-			onSubmit() {
-				console.log('submit!');
-			}
+			submitForm(formName) {
+                console.log(formName)
+            }
 		}
 		
 	}
