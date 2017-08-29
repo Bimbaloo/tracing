653b1fd9c78d17bf7592ee1d730802ad066ff71b
mazao <mazao@zjrealtech.com>
2017-08-29 14:38:35 +0800

    删除无用的配置代码

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -107,10 +107,10 @@ const config = {
     ]
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'DEV_HOST': JSON.stringify("http://192.168.227.172:8088"), //http://192.168.20.187:8080
-      'PRO_HOST': JSON.stringify("http://192.168.227.170:8087")
-    }),
+    // new webpack.DefinePlugin({
+    //   'DEV_HOST': JSON.stringify("http://192.168.227.172:8088"), //http://192.168.20.187:8080
+    //   'PRO_HOST': JSON.stringify("http://192.168.227.170:8087")
+    // }),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new CommonsChunkPlugin({
       name: 'vendors',
