3029576e8d4ec6cc417231c3468bce123445a55b
mazao <mazao@zjrealtech.com>
2017-11-10 15:39:56 +0800

    遏制路由问题修复

--- project/src/pages/restrain/app.js
+++ project/src/pages/restrain/app.js
@@ -27,53 +27,104 @@ Vue.prototype.$echarts = echarts;
 const list = r => require.ensure([], () => r(require('components/restrain/list.vue')), 'group-detail')
 const Suspicious = r => require.ensure([], () => r(require('components/restrain/suspicious.vue')), 'group-detail')
 const Chart = r => require.ensure([], () => r(require('components/restrain/chart.vue')), 'group-detail')
+const Detail = r => require.ensure([], () => r(require('components/process/detail.vue')), 'group-detail')
 
 const Process = r => require.ensure([], () => r(require('components/process/stock.vue')), 'group-detail')
-const Product = r => require.ensure([], () => r(require('components/process/product.vue')), 'group-detail')     //投产表
-const QtReport = r => require.ensure([], () => r(require('components/process/qtReport.vue')), 'group-detail')   //质检
-const QcReport = r => require.ensure([], () => r(require('components/process/qcReport.vue')), 'group-detail')   //送检检
-const FgbReport = r => require.ensure([], () => r(require('components/process/fgbReport.vue')), 'group-detail') //fgb质检
-const Tool = r => require.ensure([], () => r(require('components/process/tool.vue')), 'group-detail')           //工具
-const Event = r => require.ensure([], () => r(require('components/process/event.vue')), 'group-detail')         //事件
-const Repair = r => require.ensure([], () => r(require('components/process/repair.vue')), 'group-detail')       //维护记录
+const Product = r => require.ensure([], () => r(require('components/process/product.vue')), 'group-detail')       //投产表
+const QtReport = r => require.ensure([], () => r(require('components/process/qtReport.vue')), 'group-detail')     //质检
+const QcReport = r => require.ensure([], () => r(require('components/process/qcReport.vue')), 'group-detail')     //送检检
+const FgbReport = r => require.ensure([], () => r(require('components/process/fgbReport.vue')), 'group-detail')   //fgb质检
+const Tool = r => require.ensure([], () => r(require('components/process/tool.vue')), 'group-detail')             //工具
+const Event = r => require.ensure([], () => r(require('components/process/event.vue')), 'group-detail')           //事件
+const Repair = r => require.ensure([], () => r(require('components/process/repair.vue')), 'group-detail')         //维护记录
+const SpotReport = r => require.ensure([], () => r(require('components/process/spotReport.vue')), 'group-detail') //点检记录
+const Parameter = r => require.ensure([], () => r(require('components/process/parameter.vue')), 'group-detail')   //工艺参数
 
 
 // 定义路由
 const routes = [
   { path: '/list/:key', component: list },            // 遏制列表（未开发）
-  { path: '/restrain/1/', component: Suspicious },    // 可疑品列表
-  // { path: '/suspicious/1/', component: Process },  // 设备查询
-  { 
+  { path: '/restrain/1/', component: Suspicious,      // 可疑品列表
+    meta: {
+      title: 'restrain'
+    }
+  },    
+  {                                                   // 设备查询
     path: '/process', 
     component: Process,
     alias: '/restrain/2/',
     children: [{
       path: '',
-      component: Chart
+      component: Chart,//List
+      meta: {
+      	title: 'chart'
+      }
+    },{
+      path: 'detail',
+      component: Detail,
+      meta: {
+      	title: 'detail'
+      }
     },{
       path: 'product',
-      component: Product
+      component: Product,
+      meta: {
+      	title: 'product'
+      }
     },{//质检
       path: 'qtReport',
-      component: QtReport
+      component: QtReport,
+      meta: {
+      	title: 'qtReport'
+      }
     },{//送检
       path: 'qcReport',
-      component: QcReport
+      component: QcReport,
+      meta: {
+      	title: 'qcReport'
+      }
     },{//fgb
       path: 'fgbReport',
-      component: FgbReport
+      component: FgbReport,
+      meta: {
+      	title:'fgbReport'
+      }
     },{//工具
       path: 'tool',
-      component: Tool
+      component: Tool,
+      meta: {
+      	title: 'tool'
+      }
     },{//事件
       path: 'event',
-      component: Event
-    },{//维护记录
+      component: Event,
+      meta: {
+      	title: 'event'
+      }
+    },{//维修记录
       path: 'repair',
-      component: Repair
-    },{
+      component: Repair,
+      meta:{
+      	title: 'repair'
+      }
+    },{//点检记录
+      path: 'spotReport',
+      component: SpotReport,
+      meta: {
+      	title: 'spotReport'
+      }
+    },{//工艺参数
+      path: 'parameter',
+      component: Parameter,
+      meta: {
+      	title: 'parameter'
+      }
+    },{//遏制
       path: 'restrain',
-      component: Suspicious
+      component: Suspicious,
+      meta: {
+      	title: 'restrain'
+      }
     }]
   }
 ]
