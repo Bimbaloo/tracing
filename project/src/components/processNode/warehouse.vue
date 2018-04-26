85053578330f409810696ac572fc2cf3b397b18d
li <lishuang@zjrealtech.com>
2018-04-26 09:59:24 +0800

    修改到货登记的nodeType

--- project/src/components/processNode/warehouse.vue
+++ project/src/components/processNode/warehouse.vue
@@ -68,7 +68,7 @@ export default {
         103: '出库',
         111: '库存损益',
         112: '库存调整',
-        11113: '到货登记'
+        23: '到货登记'
       },
       materialData: {
         // filename: '',
@@ -312,7 +312,7 @@ export default {
           }
         ],
         // 到货登记。
-        1113: [
+        23: [
           {
             field: 'registration',
             headerName: '登记单号'
